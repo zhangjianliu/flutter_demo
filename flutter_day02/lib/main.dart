a5be40fbe26a37868ef1f8709a6fe1252f435e69
zhangjianliu <zhangjianliu@tuhu.cn>
2019-10-11 20:59:17 +0800

    bottomNavigationBarItem 使用示例

--- flutter_day02/lib/main.dart
+++ flutter_day02/lib/main.dart
@@ -1,36 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MaterialApp(
-      title: 'AppBar 应用按钮示例',
-      home: new LayoutDemo(),
+      title: 'BottomNavigationBar 使用示例',
+      home: new MyHomePage(),
     ));
 
-class LayoutDemo extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BottomNavigationBar 使用示例',
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 1; // 当前选中的索引
+  final _widgetOptions = [
+    Text('Index 0 : 信息'),
+    Text('Index 1: 通讯录'),
+    Text('Index 2: 发现'),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
-        title: Text('AppBar 应用按钮示例'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: '搜错',
-            onPressed: () {
-              print('搜索');
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.add),
-            tooltip: '添加',
-            onPressed: () {
-              print('添加');
-            },
-          )
-        ],
+        title: Text('BottomNavigationBar 使用示例'),
       ),
       body: Center(
-        child: Text('AppBar 应用按钮示例'),
+        child: _widgetOptions.elementAt(_selectedIndex), // 居中显示文本
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), title: Text("发现")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.contacts), title: Text("通讯录")),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), title: Text("信息")),
+        ],
+        currentIndex: _selectedIndex, // 当前选中的索引
+        fixedColor: Colors.deepOrange, // 选择中的颜色
+        onTap: _onItemTapped, //按下处理逻辑
       ),
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
